7d9373b4a417b8927c3d4f34e909e27b4c3fe15d
codewithdhruval <dhruvkarnkumar@gmail.com>
2022-08-30 11:55:23 +0530

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val a: Int = 22
+    val b: Float = 1.5f
+    val c: Char = 'T'
+    val d: String = "Dhruval Kumar"
+    val e: Boolean = true
+    val f: Double = 96.36
+    val g: Long = 338927847
+    val h: Short = -2
+    val i: Byte = 127
+
+    println("Integer value:" + a)
+    println("Float value:" + b)
+    println("Character value:" + c)
+    println("String value:" + d)
+    println("Boolean value:" + e)
+    println("Double value:" + f)
+    println("Long value:" + g)
+    println("Short value:" + h)
+    println("Byte value:" + i)
+
+}
+
+
